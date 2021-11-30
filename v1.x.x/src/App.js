f45331042ec3fa6183bc23b7d39be84bdc4eaffe
Trev Morin <trev@superklok.com>
2021-11-30 17:46:58 -0600

    Add React App

---
+++ v1.x.x/src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+	return (
+		<div>
+			<h1>React Crypto Stats</h1>
+			<p>A cryptocurrency statistical analysis app coded in React.</p>
+		</div>
+	);
+}
+
+export default App;
